066d7920140874742cc94da7e5634ce67c4acbf6
ramdirt <ramdirt@mail.ru>
2021-12-20 14:41:30 +0300

    first commit

---
+++ src/app/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <the-header></the-header>
+  </div>
+</template>
+
+<script>
+import TheHeader from '../components/TheHeader'
+  export default {
+    components: {
+      TheHeader
+    }
+  }
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="d-flex justify-content-center py-3">
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Catalog</a></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
