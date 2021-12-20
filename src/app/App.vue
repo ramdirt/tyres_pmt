a7a4e85abfafe9a0614987693fcd78f349243923
ramdirt <ramdirt@mail.ru>
2021-12-20 17:43:22 +0300

    add component AppCard

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="container">
     <the-header></the-header>
-    <router-view/>
+    <router-view />
   </div>
 
 </template>
 
 <script>
 import TheHeader from '../components/TheHeader.vue'
+import axios from 'axios'
 
 export default {
+  data() {
+    return {
+      url: 'https://tyres-pmt-default-rtdb.europe-west1.firebasedatabase.app/tyres.json',
+      tyres: []
+    }
+  },
   components: {
     TheHeader
+  },
+  methods: {
+    async loadCatalog() {
+       const res = await axios.get(this.url)
+       this.tyres.push(res.data[0])
+       console.log(this.tyres)
+    }
+  },
+  mounted() {
+    this.loadCatalog()
   }
 }
 </script>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <app-card></app-card>
   </div>
 </template>
+
+<script>
+import AppCard from '../components/AppCard.vue'
+
+  export default {
+    components: {
+      AppCard
+    }
+  }
+</script>
