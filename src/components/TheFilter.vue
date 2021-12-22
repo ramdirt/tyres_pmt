73375d885b426d53410095e601494366406bf1af
ramdirt <ramdirt@mail.ru>
2021-12-22 17:36:46 +0300

    moved filter parameters into a component

--- src/components/TheFilter.vue
+++ src/components/TheFilter.vue
@@ -2,43 +2,18 @@
 <div>
   <div class="card m-1 h-100">
     <div class="card-body" >
-        <div>
-          <h5 class="card-title">Размер покрышки</h5>
-          <div class="btn-group mb-2">
-            <button
-              href="#"
-              :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': diameter == filter}]"
-              v-for="filter in $store.getters.generateUniqueValueParameters.tyre_diameter"
-              :key="filter.id"
-              @click.prevent="changeDiameter(filter)"
-              >
-              {{filter}}
-            </button>
-            <button href="#" class="btn btn-sm btn-outline-secondary" @click.prevent="changeDiameter('')">Все</button>
-          </div>
-        </div>
-        <div>
-          <h5 class="card-title">Жесткость по Шору</h5>
-          <div class="btn-group">
-            <button
-              href="#"
-              :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': shore == filter}]"
-              v-for="filter in $store.getters.generateUniqueValueParameters.hardness_compound"
-              :key="filter.id"
-              @click.prevent="changeShore(filter)"
-              >
-              {{filter}}
-            </button>
-            <button href="#" class="btn btn-sm btn-outline-secondary" @click.prevent="changeShore('')">Все</button>
-          </div>
-        </div>
+        <the-filter-item title="Размер покрышки" filterName="tyre_diameter" @filterValue="changeDiameter"></the-filter-item>
+        <the-filter-item title="Жесткость по Шору" filterName="hardness_compound" @filterValue="changeShore"></the-filter-item>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import TheFilterItem from '../components/TheFilterItem.vue'
+
 export default {
+
   data() {
     return {
       diameter: '',
@@ -57,6 +32,9 @@ export default {
     viewType() {
       this.$store.commit('changeFilter', { diameter: this.diameter, shore: this.shore})
     }
+  },
+  components: {
+    TheFilterItem
   }
 }
 </script>
--- src/components/TheFilterItem.vue
+++ src/components/TheFilterItem.vue
@@ -1,17 +1,17 @@
 <template>
 <div>
-    <h5 class="card-title">Размер покрышки</h5>
+    <h5 class="card-title">{{ title }}</h5>
     <div class="btn-group mb-2">
     <button
-        :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': diameter == filter}]"
-        v-for="filter in $store.getters.generateUniqueValueParameters.tyre_diameter"
+        :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': parameter == filter}]"
+        v-for="filter in $store.getters.generateUniqueValueParameters[filterName]"
         :key="filter.id"
-        @click.prevent="changeDiameter(filter)"
+        @click.prevent="activeFilter(filter)"
     >{{filter}}
     </button>
     <button
         class="btn btn-sm btn-outline-secondary"
-        @click.prevent="changeDiameter('')"
+        @click.prevent="activeFilter('')"
     >Все</button>
     </div>
 </div>
@@ -20,9 +20,19 @@
 <script>
 export default {
     props: ['title', 'filterName'],
+    emits: ['filterValue'],
+    data() {
+        return {
+            parameter: ''
+        }
+    },
     methods: {
-        changeFilterParameter
+        activeFilter(filter) {
+            this.parameter = filter
+            this.$emit('filterValue', filter)
+        }
     }
+    
 }
 </script>
 
