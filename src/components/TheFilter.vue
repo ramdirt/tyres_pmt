8c970e4438d2d362a7c62127a0e78a320e181d98
ramdirt <ramdirt@mail.ru>
2021-12-22 16:48:52 +0300

    filter made

--- src/components/TheFilter.vue
+++ src/components/TheFilter.vue
@@ -2,31 +2,35 @@
 <div>
   <div class="card m-1 h-100">
     <div class="card-body" >
-        <h5 class="card-title">Размер покрышки</h5>
-        <div class="btn-group mb-2">
-          <button
-            href="#"
-            :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': diameter == filter}]"
-            v-for="filter in $store.getters.generateUniqueValueParameters.tyre_diameter"
-            :key="filter.id"
-            @click.prevent="changeDiameter(filter)"
-            >
-            {{filter}}
-          </button>
-          <button href="#" class="btn btn-sm btn-outline-secondary" @click.prevent="changeDiameter('')">Все</button>
+        <div>
+          <h5 class="card-title">Размер покрышки</h5>
+          <div class="btn-group mb-2">
+            <button
+              href="#"
+              :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': diameter == filter}]"
+              v-for="filter in $store.getters.generateUniqueValueParameters.tyre_diameter"
+              :key="filter.id"
+              @click.prevent="changeDiameter(filter)"
+              >
+              {{filter}}
+            </button>
+            <button href="#" class="btn btn-sm btn-outline-secondary" @click.prevent="changeDiameter('')">Все</button>
+          </div>
         </div>
-        <h5 class="card-title">Жесткость по Шору</h5>
-        <div class="btn-group">
-          <button
-            href="#"
-            :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': shore == filter}]"
-            v-for="filter in $store.getters.generateUniqueValueParameters.hardness_compound"
-            :key="filter.id"
-            @click.prevent="changeShore(filter)"
-            >
-            {{filter}}
-          </button>
-          <button href="#" class="btn btn-sm btn-outline-secondary" @click.prevent="changeShore('')">Все</button>
+        <div>
+          <h5 class="card-title">Жесткость по Шору</h5>
+          <div class="btn-group">
+            <button
+              href="#"
+              :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': shore == filter}]"
+              v-for="filter in $store.getters.generateUniqueValueParameters.hardness_compound"
+              :key="filter.id"
+              @click.prevent="changeShore(filter)"
+              >
+              {{filter}}
+            </button>
+            <button href="#" class="btn btn-sm btn-outline-secondary" @click.prevent="changeShore('')">Все</button>
+          </div>
         </div>
     </div>
   </div>
---
+++ src/components/TheFilterItem.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h5 class="card-title">Размер покрышки</h5>
+    <div class="btn-group mb-2">
+    <button
+        :class="['btn', 'btn-sm', 'btn-outline-secondary', {'btn-warning': diameter == filter}]"
+        v-for="filter in $store.getters.generateUniqueValueParameters.tyre_diameter"
+        :key="filter.id"
+        @click.prevent="changeDiameter(filter)"
+    >{{filter}}
+    </button>
+    <button
+        class="btn btn-sm btn-outline-secondary"
+        @click.prevent="changeDiameter('')"
+    >Все</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'filterName'],
+    methods: {
+        changeFilterParameter
+    }
+}
+</script>
+
+<style>
+
+</style>
